c9bbc0021bde8d092615c86f9c21725b578c7846
Pai-Sho <jackhessburg94@gmail.com>
2021-03-07 11:19:01 -0700

    Barebones working chat functionality

--- backend/server.py
+++ backend/server.py
@@ -5,7 +5,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 
 from flask import Flask, render_template, redirect, session, request, url_for, make_response
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 from flask_cors import CORS, cross_origin
 from db import *
 app = Flask(__name__)
@@ -13,6 +13,7 @@ app.config['SECRET_KEY']    = secrets.token_urlsafe(16)
 app.config['DATABASE']      = 'ratings.db'
 #app.config['CORS_HEADERS']  = 'Content-Type'
 socketio = SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
 CORS(app)
 
 # Flask params
@@ -35,15 +36,37 @@ API_BASE        = 'https://api.spotify.com/v1/'
 track_ratings   = {}
 track_reactions = {}
 
+
+@app.after_request
+def after_request(response):
+    #response.headers.add('Access-Control-Allow-Origin', '*')
+    #response.headers.add('Access-Control-Allow-Headers',
+    #                     'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    #response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    #response.set_cookie('same-site-cookie', 'foo', samesite='Lax');
+    # Ensure you use "add" to not overwrite existing cookie headers
+    #response.headers.add('Set-Cookie','cross-site-cookie=bar; SameSite=None;')
+    #print(response.headers)
+    return response
+
+
 # Landing page - redirects to spotify oauth
 @app.route('/', methods=['GET'])
 def home():
-    return redirect('login')
+    return redirect(url_for('login', username=secrets.token_urlsafe(6)))
 
 # Starts spotify user authentication flow
-@app.route('/login')
+@app.route('/login', methods=['GET'])
 def login():
-    session['user'] = secrets.token_urlsafe(8)
+    if 'Referer' in request.headers:
+        session['referer'] = request.headers.get('Referer')
+    else:
+        session['referer'] = request.headers.get('Host')
+
+    session['user'] = request.args.get('username')
+    #session['SameSite'] = None
+    #session['Secure'] = False
     sp_oauth = SpotifyOAuth(client_id = CLIENT_ID,
                             client_secret = CLIENT_SECRET,
                             redirect_uri = REDIRECT_URI,
@@ -52,7 +75,10 @@ def login():
 
     auth_url = sp_oauth.get_authorize_url()
 
-    return redirect(auth_url)
+    response = make_response(redirect(auth_url))
+    response.headers.add('Access-Control-Allow-Origin', session['referer'])
+
+    return response
 
 # Stores user auth tokens for session
 @app.route('/auth_callback')
@@ -66,59 +92,53 @@ def auth_callback():
     code = request.args.get('code')
     token_info = sp_oauth.get_access_token(code)
     session['token_info'] = token_info
+    session['current_track'] = None
     #session.modified = True
 
+    '''
     sp = spotipy.Spotify(auth=token_info['access_token'])
     user = sp.current_user()
     user_name = user['display_name']
     user_profile_url = user['external_urls']['spotify']
 
-    response = make_response(redirect('get_current_track_info'))
-    response.set_cookie('user_name',user_name)
-    response.set_cookie('user_profile_url',user_profile_url)
+    current_track_info = get_current_track_info()
 
-    return response
+    session['current_track'] = current_track_info['track_id']
+    '''
+
+    #response = make_response(redirect('get_user_current_track'))
+    #return ('SUCCESS',200)
+    print(session['referer']+'party')
+    return redirect(session['referer']+'party')
 
 # Queries Spotify Web API to get user's currently playing track info
 # Redirects to track-specific dashboard/chat room
-@app.route('/get_current_track_info', methods=['GET'])
-def get_current_track_info():
-    '''
-    session['token_info'], authorized = get_token(session)
-    #session.modified = True
-
-    if not authorized:
-        return redirect('login')
-    '''
-
-    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
-    response = sp.current_user_playing_track()
-
+@app.route('/get_user_current_track', methods=['GET'])
+def get_user_current_track():
     try:
-        # TODO: check if playing track (vs podcast, movie, etc)
-        track_id = response['item']['id']
-        track_name = response['item']['name']
-        album_name = response['item']['album']['name']
-        artists = [artist['name'] for artist in response['item']['artists']]
-        artwork = response['item']['album']['images'][0]['url']
-        progress_ms = response['progress_ms']
-        duration_ms = response['item']['duration_ms']
-        payload = {'track_id': track_id,
-                   'track_name': track_name,
-                   'album_name': album_name,
-                   'artists': artists,
-                   'artwork': artwork,
-                   'progress_ms': progress_ms,
-                   'duration_ms': duration_ms}
+        payload = get_current_track_info()
 
     except Exception as e:
         print(e)
         return redirect(url_for('error'))
 
-    response = make_response(redirect(url_for('dashboard', track_id=track_id)))
-    response.headers.update(payload)
+    if payload['track_id'] != session['current_track']:
+        print('Track Changed!')
+        #emit('new_track', {'track_id': payload['track_id']})
+        session['current_track'] = payload['track_id']
 
-    return response
+    print('TO BENI: ', json.dumps(payload))
+    return json.dumps(payload)
+
+@app.route('/get_track_features', methods=['GET'])
+def get_track_features():
+    track_id = request.args.get('track_id')
+    auth_manager = SpotifyClientCredentials(client_id = CLIENT_ID,
+                                            client_secret = CLIENT_SECRET)
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    audio_features = sp.audio_features(track_id)[0]
+    return json.dumps(audio_features)
 
 # App dashboard homepage - TODO: implement chat room functionality
 @app.route('/dashboard/<track_id>', methods=['GET'])
@@ -127,15 +147,13 @@ def dashboard(track_id):
                                             client_secret = CLIENT_SECRET)
     sp = spotipy.Spotify(auth_manager=auth_manager)
 
-    track = sp.track(track_id)
-    track_name = track['name']
-    artists = ', '.join([artist['name'] for artist in track['artists']])
 
     audio_features = sp.audio_features(track_id)[0]
 
-    return 'Current track info: <br/><br/>\
-            Track: {} <br/><br/> Artist: {} <br/><br/>\
-            Audio Features: <br/>{}'.format(track_name, artists, json.dumps(audio_features, indent=2).replace('\n','<br/>'))
+    #return 'Current track info: <br/><br/>\
+    #        Track: {} <br/><br/> Artist: {} <br/><br/>\
+    #        Audio Features: <br/>{}'.format(track_name, artists, json.dumps(audio_features, indent=2).replace('\n','<br/>'))
+    return render_template('chat_test.html')
 
 # Callback for track rate action
 @app.route('/rate', methods=['POST','OPTIONS'])
@@ -192,33 +210,56 @@ def get_reacts():
 # Error Page
 @app.route('/error', methods=['GET'])
 def error():
-    return 'Some kind of error happened'
+    return 'Some kind of error happened idk'
 
-@socketio.on('track refresh')
-def test_message(message):
-    emit('my response', {'data': message['data']})
+@socketio.on('connect')
+def connect():
+    print('yeet')
+    emit('connected', {'track_id':session['current_track']})
 
-# Checks to see if token is valid and gets a new token if not
-def get_token(session):
-    token_valid = False
-    token_info = session.get('token_info', {})
+@socketio.on('join')
+def on_join():
+    username = session['user']
+    room = session['current_track']
+    join_room(room)
+    print(username + ' has entered the chat.')
+    send(username + ' has entered the chat.', room=room)
 
-    # Checking if the session already has a token stored
-    # if not (session.get('token_info', False)):
-    #    token_valid = False
-    #    return token_info, token_valid
+@socketio.on('leave')
+def on_leave():
+    username = session['user']
+    room = session['current_track']
+    leave_room(room)
+    print(username + ' has left the chat.')
+    send(username + ' has left the chat.', room=room)
 
-    # Checking if token has expired
-    now = int(time.time())
-    token_expired = session.get('token_info').get('expires_at') - now < 60
+@socketio.on('send message')
+def message(data):
+    username = session['user']
+    room = session['current_track']
+    emit('broadcast message', {'user':username,'data':data['message']}, room=room)
 
-    # Refreshing token if it has expired
-    if token_expired:
-        sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id = CLI_ID, client_secret = CLI_SEC, redirect_uri = REDIRECT_URI, scope = SCOPE)
-        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+def get_current_track_info():
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    response = sp.current_user_playing_track()
 
-    token_valid = True
-    return token_info, token_valid
+    #if response != None:
+    track_id = response['item']['id']
+    track_name = response['item']['name']
+    album_name = response['item']['album']['name']
+    artists = [artist['name'] for artist in response['item']['artists']]
+    artwork = response['item']['album']['images'][0]['url']
+    progress_ms = response['progress_ms']
+    duration_ms = response['item']['duration_ms']
+    payload = {'track_id': track_id,
+               'track_name': track_name,
+               'album_name': album_name,
+               'artists': artists,
+               'artwork': artwork,
+               'progress_ms': progress_ms,
+               'duration_ms': duration_ms}
+
+    return payload
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host=FLASK_HOST, port=FLASK_PORT)
